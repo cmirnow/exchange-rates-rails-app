7fd9e941af33ea8c7e02deaaad93cae6d5476722
rishard <info@masterpro.ws>
2023-05-26 22:53:04 +0300

    highcharts as es6 module

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
-//= link highcharts/highcharts.js
-//= link highcharts/highcharts-more.js
-//= link highcharts/modules/exporting.js
-//= link highcharts/modules/export-data.js
-//= link highcharts/modules/accessibility.js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+import Highcharts from 'highcharts'
+import addMore from "highcharts/highcharts-more"
+import addExporting from "highcharts/modules/exporting"
+import addExportData from "highcharts/modules/export-data"
+import addAccessibility from "highcharts/modules/accessibility"
+addMore(Highcharts)
+addExporting(Highcharts)
+addExportData(Highcharts)
+addAccessibility(Highcharts)
+window.Highcharts = Highcharts
